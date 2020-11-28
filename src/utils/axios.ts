8de7e74397961ecfd1b8b54a71cd91833da068a5
tujvyong <yutarodd@gmail.com>
2020-11-28 19:25:43 +0900

    add: axios user update function

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -1,6 +1,11 @@
 import axios from 'axios'
 // import firebase from '../firebase/Firebase'
 
+export interface APIresponce {
+  status: number,
+  message: Error
+}
+
 export const clientAxios = axios.create({
   baseURL: "http://localhost/",
   headers: {
@@ -12,3 +17,15 @@ clientAxios.interceptors.response.use(
   response => response,
   error => error.response || error
 )
+
+export const userUpdate = async (token: string, uid: string, column: string, src: string | null) => {
+  const resp = await clientAxios.put(
+    "/user",
+    { u: uid, c: column, s: src },
+    { headers: { 'Authorization': 'Bearer ' + token }, withCredentials: true }
+  )
+  if (resp.status !== 204) {
+    console.error(resp.data.error)
+  }
+  return { status: resp.status, message: resp.data.error }
+}
