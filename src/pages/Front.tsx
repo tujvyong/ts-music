01cea028a0c9c32b2878fddfa17096adf1f05871
tujvyong <yutarodd@gmail.com>
2020-12-04 22:03:51 +0900

    fix: removed unused

--- src/pages/Front.tsx
+++ src/pages/Front.tsx
@@ -1,10 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import { createStyles, Theme, makeStyles } from '@material-ui/core/styles'
 import { Container, Typography } from '@material-ui/core'
-import { clientAxios } from "../utils/axios";
-import { useSelector } from 'react-redux';
-import { RootStore } from '../store';
-import { UserStore } from '../store/user/types';
 
 interface Props {
 
@@ -12,17 +8,6 @@ interface Props {
 
 const Front: React.FC<Props> = () => {
   const classes = useStyles()
-  const token = useSelector((state: RootStore) => state.user.token)
-  // const [state, setState] = useState<UserStore[]>([])
-
-  // useEffect(() => {
-  //   (async () => {
-  //     const res = await clientAxios.get("/userlist", { headers: { 'Authorization': 'Bearer ' + token } })
-  //     if (res) {
-  //       console.log(res)
-  //     }
-  //   })()
-  // }, [token])
 
   return (
     <Container>
